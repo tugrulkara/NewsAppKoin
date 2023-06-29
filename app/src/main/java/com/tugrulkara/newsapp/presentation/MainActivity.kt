60cdb35aad832df2b19eb5d3457d0ea869c55b08
tugrulkara <74429693+tugrulkara@users.noreply.github.com>
2023-06-29 20:29:55 +0300

    other changes and finish app

--- app/src/main/java/com/tugrulkara/newsapp/presentation/MainActivity.kt
+++ app/src/main/java/com/tugrulkara/newsapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tugrulkara.newsapp
+package com.tugrulkara.newsapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tugrulkara.newsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/tugrulkara/newsapp/util/Constants.kt
+++ app/src/main/java/com/tugrulkara/newsapp/util/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
 
     const val BASE_URL="https://newsapi.org/v2/"
     const val API_KEY="acdac7ca321a42f6b6d14d1211f8970c"
-    const val API_COUNTRY="tr"
+    const val API_COUNTRY="us"
 }
