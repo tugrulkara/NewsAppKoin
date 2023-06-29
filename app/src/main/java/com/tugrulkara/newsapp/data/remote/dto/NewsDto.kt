58d2c63fe3b721fd5921c3a000a9dc5e06d16dfa
tugrulkara <74429693+tugrulkara@users.noreply.github.com>
2023-06-29 20:22:16 +0300

    update NewsDto and delete Model

--- app/src/main/java/com/tugrulkara/newsapp/data/remote/dto/NewsDto.kt
+++ app/src/main/java/com/tugrulkara/newsapp/data/remote/dto/NewsDto.kt
@@ -1,21 +1,7 @@
 package com.tugrulkara.newsapp.data.remote.dto
 
-import com.tugrulkara.newsapp.domain.model.ArticleModel
-
 data class NewsDto (
     val status: String,
     val totalResults: Long,
     val articles: List<Article>
 )
-
-fun NewsDto.toArticleList(): List<ArticleModel>{
-    return articles.map {article ->
-        ArticleModel(
-            title = article.title,
-            description = article.description,
-            url = article.url,
-            urlToImage = article.urlToImage,
-            publishedAt = article.publishedAt,
-            content = article.content)
-    }
-}
